d8eeb7bb3abbbc6d5da5033860b427012e3bb9ca
Simon Johann Skødt <simon@skodt.dk>
2025-02-10 21:41:49 +0100

    refactor: rename file

--- concurrect.c
+++
@@ -1,5 +0,0 @@
-#include "partitioning.h"
-
-void concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads) {
-    return 0;
-}
---
+++ concurrent.c
@@ -0,0 +1,6 @@
+#include "partitioning.h"
+
+int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads) {
+    printf("Ran concurrent partitioning\n");
+    return 0;
+}
