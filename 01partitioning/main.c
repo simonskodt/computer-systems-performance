68139caffed9e96a6baa68121bbe186d7512a461
Simon Johann Skødt <simon@skodt.dk>
2025-02-11 16:26:00 +0100

    feat: optional argument for n_tuples

--- 01partitioning/main.c
+++ 01partitioning/main.c
@@ -1,26 +1,36 @@
 #include "partitioning.h"
 
+// Function prototypes
 void print_usage();
 Tuple* setup_tuples(size_t n_tuples);
+pthread_t* setup_threads(size_t n_threads);
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
+    if (argc != 4 && argc != 5) {
         print_usage();
         return EXIT_FAILURE;
     }
 
     char *algorithm = argv[1];
-    size_t n_tuples = atoi(argv[2]);
-    size_t n_hash_bits = atoi(argv[3]);
-    size_t n_threads = atoi(argv[4]);
+    size_t n_hash_bits = atoi(argv[2]);
+    size_t n_threads = atoi(argv[3]);
 
-    if (n_tuples <= 0 || n_hash_bits <= 0 || n_threads <= 0) {
+    // Default value: 2^(24) tuples
+    size_t n_tuples = 1ULL << 24;
+    if (argc == 5) {
+        n_tuples = atoi(argv[4]);
+    }
+
+    if (n_hash_bits <= 0 || n_threads <= 0 || n_tuples <= 0) {
         fprintf(stderr, "Error: All arguments must be positive\n");
         return EXIT_FAILURE;
     }
 
+    // Setup tuples and threads
     Tuple* tuples = setup_tuples(n_tuples);
+    pthread_t* threads = setup_threads(n_threads);
 
+    // Match on selected algorithm
     if (strcmp(algorithm, "independent") == 0) {
         independent_output(tuples, n_tuples, n_hash_bits, n_threads);
     } else if (strcmp(algorithm, "concurrent") == 0) {
@@ -37,23 +47,42 @@ int main(int argc, char *argv[]) {
 
 void print_usage() {
     printf("Usage: ./partition <algorithm> <n_tuples> <n_hash_bits> <n_threads>\n");
-    printf("  algorithm:   'independent' or 'concurrent'\n");
-    printf("  n_tuples:    number of tuples to partition\n");
+    printf("  algorithm:   'ind' for independent or 'con' for concurrent\n");
     printf("  n_hash_bits: number of hash bits to use\n");
     printf("  n_threads:   number of threads to use\n");
+    printf("  n_tuples:    number of tuples to partition (optional, default 2^(24)\n");
 }
 
 Tuple* setup_tuples(size_t n_tuples) {
+    // Check for overflow of total byte allocation
+    uint64_t total_bytes = (u_int64_t) n_tuples * sizeof(Tuple);
+    if (total_bytes / sizeof(Tuple) != n_tuples) {
+        fprintf(stderr, "Error: Overflow for total size of tuples\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allocate tuples
     Tuple* tuples = malloc(n_tuples * sizeof(Tuple));
     if (tuples == NULL) {
         perror("Could not allocate memory for tuples");
         exit(EXIT_FAILURE);
     }
 
+    // Generate key value pair
     for (size_t i = 0; i < n_tuples; i++) {
-        tuples[i].key = i;
+        tuples[i].key = i; // might need different distribution (shuffle) later on
         tuples[i].value = rand() % 1000;
     }
 
     return tuples;
+}
+
+pthread_t* setup_threads(size_t n_threads) {
+    pthread_t* threads = malloc(n_threads * sizeof(pthread_t));
+    if (threads == NULL) {
+        perror("Could not allocate memory for threads");
+        exit(EXIT_FAILURE);
+    }
+
+    return threads;
 }
