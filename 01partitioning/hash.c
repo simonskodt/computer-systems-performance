3417a1fb1b0d14d5679bc8bb4b0b0a043944ca75
Simon Johann Skødt <simon@skodt.dk>
2025-02-11 16:26:28 +0100

    docs: add comments to understand partition calc

--- 01partitioning/hash.c
+++ 01partitioning/hash.c
@@ -1,6 +1,9 @@
 #include "partitioning.h"
 
 int mod_hashing(uint64_t key, size_t hash_bits) {
+    // Partitions are calculated by 2^(hash_bits), example:
+    // 1      = 0000 0001
+    // 1 << 4 = 0001 0000 = 16 = 2^4
     size_t n_partitions = 1 << hash_bits;
     return key % n_partitions;
 }
