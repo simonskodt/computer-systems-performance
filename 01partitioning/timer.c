05781edab2dd136a2f005fdd3fa05e0abe2d83f2
Simon Johann Skødt <simon@skodt.dk>
2025-02-11 18:24:55 +0100

    feat: add timer with start and end timer api

---
+++ 01partitioning/timer.c
@@ -0,0 +1,15 @@
+#include "timer.h"
+
+struct timespec start_timer(void) {
+    struct timespec start_time;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+    return start_time;
+}
+
+long end_timer(struct timespec start_time) {
+    struct timespec end_time;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
+    long elapsed_time_ms = (end_time.tv_sec - start_time.tv_sec) * 1000 + 
+                           (end_time.tv_nsec - start_time.tv_nsec) / 1000000;
+    return elapsed_time_ms;
+}
