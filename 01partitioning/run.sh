8eb8784569e14ab3a9300878fadf5277e954621e
Simon Johann Skødt <simon@skodt.dk>
2025-02-11 16:14:27 +0100

    fix: handle fourth argument for partitioning

--- 01partitioning/run.sh
+++ 01partitioning/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Avoids same output when files have not changed
-make 2>&1 | grep -v "make: Nothing to be done for \`all'."
+make 2>&1 | grep -v "make: Nothing to be done for [\`']all[\`']."
 
 case "$1" in
-    "ind")
-        ./build/partition independent "$2" "$3" "$4"
+    "ind"|"independent")
+        if [ -n "$4" ]; then
+            ./build/partition independent "$2" "$3" "$4"
+        else
+            ./build/partition independent "$2" "$3"
+        fi
         ;;
-    "con")
-        ./build/partition concurrent "$2" "$3" "$4"
+    "con"|"concurrent")
+        if [ -n "$4" ]; then
+            ./build/partition concurrent "$2" "$3" "$4"
+        else
+            ./build/partition concurrent "$2" "$3"
+        fi
         ;;
     *)
         ./build/partition
