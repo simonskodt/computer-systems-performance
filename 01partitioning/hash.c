a69735bcedce8ad0ded6b9cad907141a4f121d76
Simon Johann Skødt <simon@skodt.dk>
2025-02-18 13:55:44 +0100

    fix: update mod_hashing to use uint64_t for partition count and add debug output

--- 01partitioning/hash.c
+++ 01partitioning/hash.c
@@ -1,9 +1,10 @@
 #include "hash.h"
 
-int mod_hashing(uint64_t key, size_t hash_bits) {
+uint64_t mod_hashing(uint64_t key, size_t hash_bits) {
     // Partitions are calculated by 2^(hash_bits), example:
     // 1      = 0000 0001
     // 1 << 4 = 0001 0000 = 16 = 2^4
-    size_t n_partitions = 1 << hash_bits;
+    uint64_t n_partitions = 1 << hash_bits;
+    printf("n_partitions in mod_hashing: %llu\n", n_partitions);
     return key % n_partitions;
 }
