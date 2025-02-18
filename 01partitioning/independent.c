678bcce82757c59b31e9b7620a3a749db581e333
Simon Johann Skødt <simon@skodt.dk>
2025-02-18 12:14:13 +0100

    feat: concurrent and independent partitioning with threading support

--- 01partitioning/independent.c
+++ 01partitioning/independent.c
@@ -1,6 +1,7 @@
 #include "partitioning.h"
+#include "colors.h"
 
-int independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads) {
-    printf("Ran independent partitioning\n");
+int independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads, pthread_t* threads) {
+    printf(COLOR_GREEN "Running independent partitioning..." COLOR_RESET "\n");
     return EXIT_SUCCESS;
 }
