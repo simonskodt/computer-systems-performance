ed1e62d2ae43b0ae88388e98e06e7a25a82cb2d2
Simon Johann Skødt <simon@skodt.dk>
2025-02-18 12:14:13 +0100

    feat: concurrent and independent partitioning with threading support

--- 01partitioning/concurrent.c
+++ 01partitioning/concurrent.c
@@ -1,6 +1,62 @@
 #include "partitioning.h"
 
-int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads) {
-    printf("Ran concurrent partitioning\n");
+typedef struct {
+    int thread_id;
+    Tuple *tuples;
+    size_t n_hash_bits;
+} ConcurrentThread;
+
+void* concurrent_thread_function(void* args);
+
+int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads, pthread_t* threads) {
+    printf(COLOR_GREEN "\nRunning concurrent partitioning..." COLOR_RESET "\n");
+
+    int n_partitions = COMPUTE_PARTITIONS(n_hash_bits);
+    size_t tuples_per_thread = n_tuples / n_threads;  // uniform distribution
+
+
+    ConcurrentThread* thread_args = malloc(n_threads * sizeof(ConcurrentThread));
+    if (thread_args == NULL) {
+        perror("Could not allocate memory for thread instances");
+        return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < n_threads; i++) {
+        thread_args[i].thread_id = i;
+        thread_args[i].tuples = tuples;
+        thread_args[i].n_hash_bits = n_hash_bits;
+    
+        if (pthread_create(&threads[i], NULL, concurrent_thread_function, &thread_args[i]) != 0) {
+            perror("Could not create thread");
+            free(thread_args);
+            return EXIT_FAILURE;
+        }
+    }
+    
+
+    for (size_t i = 0; i < n_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // Free memory
+    free(threads);
+
     return EXIT_SUCCESS;
+}
+
+void* concurrent_thread_function(void* args) {
+    ConcurrentThread* c_thread = (ConcurrentThread*) args;
+
+    size_t start_index = 0; // replace
+    size_t end_index = 1;   // replace
+
+    for (size_t i = start_index; i < end_index; i++) {
+        int partition_index = mod_hashing(
+            c_thread->tuples[i].key,
+            c_thread->n_hash_bits
+        );
+
+    }
+
+    return NULL;
 }
--- 01partitioning/partitioning.h
+++ 01partitioning/partitioning.h
@@ -5,13 +5,17 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include "colors.h"
+#include <hash.h>
+
+#define COMPUTE_PARTITIONS(hash_bit) (1 << hash_bit);
 
 typedef struct {
     uint64_t key;
     uint64_t value;
 } Tuple;
 
-int independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads);
-int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads);
+int independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads, pthread_t* threads);
+int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads, pthread_t* threads);
 
 #endif // PARTITIONING_H
