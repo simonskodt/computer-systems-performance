b80e4a0c233bcdcae438ac9c7808f0f00917139a
Simon Johann Skødt <simon@skodt.dk>
2025-03-04 21:32:06 +0100

    refactor: change return type of output functions to long and include necessary headers

--- 01partitioning/partitioning.h
+++ 01partitioning/partitioning.h
@@ -6,8 +6,10 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
 #include "colors.h"
 #include "hash.h"
+#include "timer.h"
 
 #define COMPUTE_PARTITIONS(hash_bit) (1 << hash_bit);
 #define THROUGHPUT(tuples, milliseconds) ((size_t)(tuples / ((milliseconds) / 1000.0)));
@@ -17,7 +19,7 @@ typedef struct {
     uint64_t value;
 } Tuple;
 
-int independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads);
-int concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads, pthread_t* threads);
+long independent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads);
+long concurrent_output(Tuple *tuples, size_t n_tuples, size_t n_hash_bits, size_t n_threads);
 
 #endif // PARTITIONING_H
