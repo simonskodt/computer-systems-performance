18f9cbfec2d5ac80987e2cad5f45ca485e796cb8
Kusmar00 <markusraunjohansen@gmail.com>
2025-05-21 18:11:58 +0200

    feat: update make_graphs.py to work with new data format

--- 02_db_oltp_vs_olap/src/make_graphs.py
+++ 02_db_oltp_vs_olap/src/make_graphs.py
@@ -5,6 +5,7 @@ import sys
 import math
 import matplotlib.pyplot as plt
 
+
 def read_latencies(file_path: str) -> Dict[int, Dict[str, float]]:
     latencies: Dict[int, Dict[str, float]] = {}
     with open(file_path, "r") as file:
@@ -14,16 +15,34 @@ def read_latencies(file_path: str) -> Dict[int, Dict[str, float]]:
                 query_part, time_part = parts
                 query_number = int(query_part.split()[1])
                 sqlite_time, duckdb_time = time_part.split(",")
-                latencies[query_number] = {
-                    "sqlite": float(sqlite_time.split("=")[1].split('s')[0]),
-                    "duckdb": float(duckdb_time.split("=")[1].split('s')[0]),
-                }
+                if latencies.get(query_number) is None:
+                    latencies[query_number] = {
+                        "sqlite": [float(sqlite_time.split("=")[1].split('s')[0])],
+                        "duckdb": [float(duckdb_time.split("=")[1].split('s')[0])],
+                    }
+                else:
+                    latencies[query_number]["sqlite"].append(
+                        float(sqlite_time.split("=")[1].split('s')[0]))
+                    latencies[query_number]["duckdb"].append(
+                        float(duckdb_time.split("=")[1].split('s')[0]))
 
-    print("Latencies:")
-    pprint.pp(latencies)
     return latencies
 
-def create_bar_graph(latencies: Dict[int, Dict[str, float]]) -> None:
+
+def calculate_average(latencies: Dict[int, Dict[str, float]]) -> Dict[int, Dict[str, float]]:
+    for query_number, times in latencies.items():
+        sqlite_times = times["sqlite"]
+        duckdb_times = times["duckdb"]
+        avg_sqlite_time = sum(sqlite_times) / len(sqlite_times)
+        avg_duckdb_time = sum(duckdb_times) / len(duckdb_times)
+        latencies[query_number] = {
+            "sqlite": avg_sqlite_time,
+            "duckdb": avg_duckdb_time
+        }
+    return latencies
+
+
+def create_bar_graph(latencies: Dict[int, Dict[str, float]], savename: str) -> None:
     queries = list(latencies.keys())
     sqlite_times = [latencies[q]["sqlite"] for q in queries]
     duckdb_times = [latencies[q]["duckdb"] for q in queries]
@@ -43,9 +62,8 @@ def create_bar_graph(latencies: Dict[int, Dict[str, float]]) -> None:
     plt.yscale('log')
     plt.legend()
     plt.tight_layout()
-    plt.savefig("latencies.png")
-    print("Graph saved as latencies.png")
-    plt.show()
+    plt.savefig(savename)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -54,4 +72,17 @@ if __name__ == "__main__":
 
     latencies_file: str = sys.argv[1]
     latencies = read_latencies(latencies_file)
-    create_bar_graph(latencies)
+    print()
+    print("Latencies:")
+    pprint.pp(latencies)
+    print()
+
+    avg_latencies = calculate_average(latencies)
+    print("Average Latencies:")
+    pprint.pp(avg_latencies)
+    print()
+
+    savename = latencies_file.split(".")[-3].split("/")[-1] + "_graph.png"
+    create_bar_graph(avg_latencies, savename)
+    print("Graph saved as " + savename)
+    print()
