1a38881a3e970751ab8a02aeac3c2554a3690782
Simon Johann Skødt <simon@skodt.dk>
2025-05-21 11:52:24 +0200

    refactor: uncomment TPC-H and use 2 different scale factors

--- 02_db_oltp_vs_olap/src/run-benchmark.sh
+++ 02_db_oltp_vs_olap/src/run-benchmark.sh
@@ -6,20 +6,21 @@ BENCHMARK_SCRIPT="$SCRIPT_DIR/db_benchmark.py"
 OUTPUT_DIR="$SCRIPT_DIR/../out"
 
 SCALE_FACTORS=(1 2 5)
+SCALE_FACTORS_2=(1 5 20)
 NUM_ITERATIONS=5
 
 # Run benchmarks for TPC-H
-#for sf in "${SCALE_FACTORS[@]}"; do
-#    for ((i=0; i<NUM_ITERATIONS; i++)); do
-#        if [ $i -eq 0 ]; then
-#            # First iteration without --reuse
-#            python3 "$BENCHMARK_SCRIPT" --benchmark TPC_H --sf "$sf"
-#        else
-#            # Subsequent iterations with --reuse
-#            python3 "$BENCHMARK_SCRIPT" --benchmark TPC_H --sf "$sf" --reuse
-#        fi
-#    done
-#done
+for sf in "${SCALE_FACTORS[@]}"; do
+    for ((i=0; i<NUM_ITERATIONS; i++)); do
+        if [ $i -eq 0 ]; then
+            # First iteration without --reuse
+            python3 "$BENCHMARK_SCRIPT" --benchmark TPC_H --sf "$sf"
+        else
+            # Subsequent iterations with --reuse
+            python3 "$BENCHMARK_SCRIPT" --benchmark TPC_H --sf "$sf" --reuse
+        fi
+    done
+done
 
 # Run benchmarks for TPC-C
 for ((i=0; i<NUM_ITERATIONS; i++)); do
